d62444af19502a49b08d9cacf3ec07ec1e982b47
Yi Qiao <atrusqiao@gmail.com>
2016-06-23 17:02:23 -0600

    added fetch-images.sh to pre-fetch or update docker images

---
+++ fetch-images.sh
@@ -0,0 +1,15 @@
+#/bin/bash 
+docker pull nginx:stable-alpine
+docker pull qiaoy/iobio.geneinfo
+docker pull qiaoy/iobio.gene2pheno
+docker pull qiaoy/iobio.hpo
+docker pull qiaoy/iobio.coverage
+docker pull qiaoy/iobio.samtools_od
+docker pull qiaoy/iobio.freebayes
+docker pull qiaoy/iobio.vcflib
+docker pull qiaoy/iobio.vt
+docker pull qiaoy/iobio.tabix_od
+docker pull qiaoy/iobio.vcfdepther
+docker pull qiaoy/iobio.af
+docker pull qiaoy/iobio.vep
+docker pull qiaoy/iobio.ctgapndr
