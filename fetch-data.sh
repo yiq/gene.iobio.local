e76b74b5fff8a9374c5531a296ae267f32463ad0
Yi Qiao <atrusqiao@gmail.com>
2016-08-08 21:08:00 +0000

    unified fetch-data.sh to use .tar.gz

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-fetch_gz() {
+fetch() {
     if [ ! -e data-vol/.${1}.fetched ]; then
         echo "Fetching ${1}"
-        curl -L\# https://s3.amazonaws.com/iobio/assets/data-vol/v1.0/${1}.tar.gz | tar -C data-vol -xzf -
+        curl -L\# https://s3.amazonaws.com/iobio/assets/data-vol/v2.0/${1}.tar.gz | tar -C data-vol -xzf -
         if [ $? -eq 0 ]; then touch data-vol/.${1}.fetched; fi
     fi
 }
 
-fetch_bz2() {
-    if [ ! -e data-vol/.${1}.fetched ]; then
-        echo "Fetching ${1}"
-        curl -L\# https://s3.amazonaws.com/iobio/assets/data-vol/v1.0/${1}.tar.bz2 | tar -C data-vol -xjf -
-        if [ $? -eq 0 ]; then touch data-vol/.${1}.fetched; fi
-    fi
-}
-
-fetch_gz afdata
-fetch_gz gene2pheno
-fetch_gz geneinfo
-fetch_gz hpo
-fetch_gz ref
-fetch_bz2 vep-cache
-fetch_gz local_cache
+fetch afdata
+fetch gene2pheno
+fetch geneinfo
+fetch hpo
+fetch ref
+fetch vep-cache
+fetch local_cache
 
 
